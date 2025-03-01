eb1b0a63672434bbeb8e6c4536c23a8d4a6b30cf
Amandeep-rawat <ar0466467@gmail.com>
2025-03-01 20:16:30 +0530

    improved article detailed page

--- components/dashboard/articles/article-detailed-page.tsx
+++ components/dashboard/articles/article-detailed-page.tsx
@@ -87,13 +87,7 @@ interface GridItemProps {
     return (
       <li className={`min-h-[14rem] list-none `}>
         <div className="relative h-full rounded-2.5xl border  p-2  md:rounded-3xl md:p-3">
-          <GlowingEffect
-            spread={40}
-            glow={true}
-            disabled={false}
-            proximity={64}
-            inactiveZone={0.01}
-          />
+          
           <div className="relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl border-0.75 p-6  dark:shadow-[0px_0px_27px_0px_#2D2D2D] md:p-6">
             <div className="relative flex flex-1 flex-col justify-between gap-3">
                 <div className='flex justify-between items-center'>
