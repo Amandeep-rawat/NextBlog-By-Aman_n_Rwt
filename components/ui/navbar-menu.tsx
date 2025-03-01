16437b9f526600bd2c7779cb119dc1078699f74c
Amandeep-rawat <ar0466467@gmail.com>
2025-03-01 20:20:40 +0530

    modfied nav

--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -147,11 +147,12 @@ export const Menu = ({
                   <div className="w-full flex font-semibold text-white flex-col gap-3">
                     <Link className='w-full' href={"/dashboard"}><Button className='w-full justify-start' variant={"ghost"}><LayoutDashboard />Dashboard</Button></Link>
                     <Link className='w-full' href={"/about"}><Button className='w-full justify-start' variant={"ghost"}><Info />About</Button></Link>
-                    <Link className='w-full' href={"/dashboard"}><Button className='w-full justify-start' variant={"ghost"}><FileText />Articles</Button></Link>
+                    <Link className='w-full' href={"/articles"}><Button className='w-full justify-start' variant={"ghost"}><FileText />Articles</Button></Link>
                   </div>
 
                   <div className="flex flex-col items-center gap-y-3">
 
+                  <SignedOut>
 
                     <button className="inline-flex h-10 w-full animate-shimmer  items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-5 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                       Login
@@ -161,6 +162,7 @@ export const Menu = ({
 
                       Signup
                     </button>
+                  </SignedOut>
 
 
 
